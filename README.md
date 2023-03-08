217f8cbfa6385d309bd5f47ed417cfcc318a0826
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-08 14:52:59 +0300

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+Open [https://automedon.github.io/Merkury/](https://automedon.github.io/Merkury/) to view it in the browser.
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+
+
+const AppContainer = styled.div`
+  width: 100%;
+  margin: 0 auto;
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      aaa
+    </AppContainer>
+  );
+}
+
+export default App;
